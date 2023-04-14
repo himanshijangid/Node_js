c73d33c325c84d371f2767309ec76cade4ee5150
Himanshi Jangid <himanshijangid444@gmail.com>
2023-04-14 14:41:47 +0530

    day 11

---
+++ day11/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const rootDir = require('./util/path')
+const app = express();
+
+
+const adminRouters = require('./routes/admin');
+const shopRouters = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin',adminRouters);
+app.use(shopRouters);
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
+})
+
+
+app.listen(8080);
