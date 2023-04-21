5d56e1295fd618a6d62e10d478e92ff3616e0f08
Himanshi Jangid <himanshijangid444@gmail.com>
2023-04-21 14:34:23 +0530

    YUHJJH

---
+++ day15/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRouters = require('./routes/shop');
+
+
+app.set('view engine', 'ejs');
+app.set('views' , 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRouters);
+
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+})
+
+
+app.listen(3000);
