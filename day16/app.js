f6ebbc11d8f4b310bb1ff1fef85878ec0e737fcb
Himanshi Jangid <himanshijangid444@gmail.com>
2023-04-25 15:50:32 +0530

    uiuu

--- day16/app.js
+++ day16/app.js
@@ -1,25 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const app = express();
-const errorController  = require('./controllers/error')
 
-const adminRoutes = require('./routes/admin')
-const shopRouters = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views' , 'views');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-
-app.use('/admin',adminRoutes);
-app.use(shopRouters);
-
-
-app.use(errorController.get404)
-
+app.use(errorController.get404);
 
 app.listen(5000);
