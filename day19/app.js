00dd815d244ee047cadf0b9d9032e09e23f4d490
Himanshi Jangid <himanshijangid444@gmail.com>
2023-04-27 15:49:38 +0530

    ghfjkthjjy

--- day19/app.js
+++ day19/app.js
@@ -21,6 +21,5 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(5000);
-
-console.log("server listing 5000")
+app.listen(8080);
+console.log('Server listening on : 8080');
--- day19/controllers/shop.js
+++ day19/controllers/shop.js
@@ -1,23 +1,35 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop/product-list', {
+    res.render('shop/product-list',
+     {
       prods: products,
       pageTitle: 'All Products',
       path: '/products',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
     });
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail',
+     {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  })
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop/index', {
+    res.render('shop/index', 
+    {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
@@ -30,14 +42,35 @@ exports.getIndex = (req, res, next) => {
 
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
+  res.render('shop/cart',
+   {
     path: '/cart',
     pageTitle: 'Your Cart'
   })
 }
 
+exports.postCart = (req, res, next) => {
+
+  const prodId = req.body.productId;
+
+  Product.findById(prodId, product => {
+
+    Cart.addProduct(prodId, product.price);
+  })                                       
+  res.redirect('/cart');
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', 
+  {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  })
+}
+
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
+  res.render('shop/checkout',
+   {
     path: '/checkout',
     pageTitle: 'Checkout'
   })
