7b835f1cf5b53d3716fd9ccacf85391b59bcca4c
Himanshi Jangid <himanshijangid444@gmail.com>
2023-04-28 15:57:48 +0530

    fgfbgf

--- day20/controllers/admin.js
+++ day20/controllers/admin.js
@@ -38,7 +38,20 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-    
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedProduct = new Product(
+        prodId,
+        updatedTitle,
+        updatedPrice,
+        updatedImageUrl,
+        updatedDescription
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) => {
