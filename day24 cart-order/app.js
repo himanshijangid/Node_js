04d1e1fd2cf6628eec9366b020efee7cdea64eb9
Himanshi Jangid <himanshijangid444@gmail.com>
2023-05-18 14:19:03 +0530

    yjyu

--- day24 cart-order/app.js
+++ day24 cart-order/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
+    User.findByPk(3)
         .then(user => {
             req.user = user;
             next();
