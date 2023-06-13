2f4f416a118c4ea371e0fc53ac435c4ce7327923
Himanshi Jangid <himanshijangid444@gmail.com>
2023-06-13 15:52:01 +0530

    mjhh

---
+++ day34 session/app.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+// const mongoConnect = require('./util/database').mongoConnect;
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('6475daf920fc988a472c1a0c')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+app.use(errorController.get404);
+
+mongoose.connect('mongodb+srv://himanshijangid444:himanshijangid444@cluster0.m0npxln.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'Ram',
+        email: 'ram@gmail.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  });
+  app.listen(3000);
+  console.log('Database Connected...')
+})
+.catch(err => { console.log(err) })
