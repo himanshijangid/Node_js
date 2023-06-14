677a0c52a468e213df982436d42cc6b8ae97e4df
Himanshi Jangid <himanshijangid444@gmail.com>
2023-06-14 16:22:15 +0530

    session

--- day35 session moduel/controllers/auth.js
+++ day35 session moduel/controllers/auth.js
@@ -1,32 +1,28 @@
+const User = require('../models/user');
 exports.getLogin = (req, res, next) => {
-    // const isLoggedIn = req.get('Cookie').split('=')[1]
-    // console.log(req.get('Cookie'))
-    // console.log(req.get('Cookie').split('=')[1])
-    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: false
+        isAuthenticated: req.session.isLoggedIn
     });
 }
 
-exports.postLogin = (req, res, next) =>{
+exports.postLogin = (req, res, next) => {
     User.findById('6475daf920fc988a472c1a0c')
-      .then(user => {
-          req.session.isLoggedIn = true;
-        req.session.user = user;
-        res.redirect('/');
-       
-      })
-      .catch(err => console.log(err));
-  }
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(error => {
+                console.log(error);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+}
 
-
-  
 exports.postLogout = (req, res, next) => {
-     req.session.destroy (error => {
+    req.session.destroy(error => {
         console.log(error);
-         res.redirect('/');
-     }
- )
+        res.redirect('/');
+    })
 }
