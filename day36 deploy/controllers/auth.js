9aa6bea84d3ccbdc036bc85cbce7acdb970bf7d0
Himanshi Jangid <himanshijangid444@gmail.com>
2023-06-15 15:04:47 +0530

    htht

---
+++ day36 deploy/controllers/auth.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: req.session.isLoggedIn
+    });
+}
+
+exports.postLogin = (req, res, next) => {
+    User.findById('646f43d0a55fc76aaf5a4e70')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    })
+}
