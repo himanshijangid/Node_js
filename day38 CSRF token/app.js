b6fa1e599277d472ceeed783bb9c0085dc95c441
Himanshi Jangid <himanshijangid444@gmail.com>
2023-06-21 14:28:57 +0530

    day 38 final

--- day38 CSRF token/app.js
+++ day38 CSRF token/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-
+const csrf = require('csurf')
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -17,6 +17,8 @@ const store = new MongoDBStore({
   uri: process.env.MONGODB_CONNECT_URI,
   collection: 'sessions'
 })
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -36,6 +38,9 @@ app.use(
   })
 )
 
+app.use (csrfProtection);
+
+
 app.use((req, res, next) => {
   if(!req.session.user){
     return next();
@@ -44,6 +49,12 @@ app.use((req, res, next) => {
     req.user = user
       next()
   }).catch(err=>console.log(err))
+})
+  
+app.use((req, res , next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 })
 
 
