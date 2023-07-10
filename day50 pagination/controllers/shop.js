5fe8825c1b708835f9497cb5a820697cb218a84a
Himanshi Jangid <himanshijangid444@gmail.com>
2023-07-10 16:52:33 +0530

    day 50 complete

--- day50 pagination/controllers/shop.js
+++ day50 pagination/controllers/shop.js
@@ -1,22 +1,35 @@
 const fs = require('fs');
-const path = require('path')
-const PDFDocument = require('pdfkit')
+const path = require('path');
+
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
-const product = require('../models/product');
-const ITEMS_PER_PAGE = 2;
+
+const ITEMS_PER_PAGE = 3;
 
 exports.getProducts = (req, res, next) => {
-  // const page = req.query.page
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-  // .skip((page - 1) * ITEMS_PER_PAGE)
-  // .limit(ITEMS_PER_PAGE)
-    .then(products => {
-      console.log(products);
-      res.render('shop/product-list', {
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    }).then(products => {
+      res.render('shop/index', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Shop',
+        path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -42,36 +55,35 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
-  let totalItems ; 
+  const page = +req.query.page || 1;
+  let totalItems;
+  
   Product.find()
-  .countDocuments()
-  .then(numProducts => {
-    totalItems = numProducts;
-   return Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
-
-  })
-    .then(products => {
-      res.render('shop/index', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        currentPage : page ,
-        hasNextPage :ITEMS_PER_PAGE * page < totalItems,
-        hasPriviousPage : page > 1,
-        nextPage : page + 1,
-        priviousPage : page - 1 ,
-        lastPage : Math.ceil(totalItems / ITEMS_PER_PAGE)
-
+    .countDocuments()
+    .then(numProducts => { 
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    }).then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
       {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        // const error = new Error(err);
+        // error.httpStatusCode = 500;
+        // return next(error);
+        console.log(err);
 };
     });
 };
@@ -80,7 +92,6 @@ exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     .then(user => {
-   
       const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
@@ -124,7 +135,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    
     .then(user => {
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
@@ -167,6 +177,7 @@ exports.getOrders = (req, res, next) => {
 });
 };
 
+
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
   Order.findById(orderId)
@@ -197,33 +208,27 @@ exports.getInvoice = (req, res, next) => {
           ' - ' +
           prod.quantity +
           ' X ' +
-          ' ₹  ' +
+          ' $' +
           prod.product.price
         )
       });
       pdfDoc.text('-----');
-      pdfDoc.fontSize(20).text('Total Price: ₹ ' + totalPrice);
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
     pdfDoc.end();
-
-    // fs.readFile(invoicePath,(error,data)=>{
-    //   if(error){
-    //     return next(error);
-    //   }else {
-    //     const file = fs.createReadStream(invoicePath);
-    //     res.setHeader('Content-Type', 'application/pdf');
-    //     res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-    //     res.send(data);
-    //   }
-    // })
-    // const file = fs.createReadStream(invoicePath);
-    // res.setHeader('content-type','application/pdf');
-    // res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-    // file.pipe(res);
-  })
-  .catch(err => {
-    const error = new Error(err);
-    error.httpStatusCode = 500;
-    return next(error);
-});
+  //   fs.readFile(invoicePath,(err,data)=>{
+  //     if(err){
+  //       return next(err);
+  //     }
+  //     res.setHeader('Content-Type', 'application/pdf');
+  //     res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+  //     res.send(data);
+  // })
+  // const file = fs.createReadStream(invoicePath);
+  //     res.setHeader('Content-Type', 'application/pdf');
+  //     res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+  //   file.pipe(res);  
+    
+    })
+    .catch(err => next(err));
 }
